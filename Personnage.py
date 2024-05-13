50429ab4a48236663907bb31cefc9a538833a254
Guillaume D <guil.duchesne@gmail.com>
2024-05-13 15:20:25 +0200

    classe personnage
    Classe personnage abstraite (terminer)
    (A voir si il faut la mettre a jour plus tard)

---
+++ Personnage.py
@@ -0,0 +1,16 @@
+
+
+class Personnage:
+    def __init__(self):
+        self._nom = ""
+        self._vie = 100
+        self._degats = 0
+        self._position = (0 ,0)
+        self._inventaire = []
+
+    
+    def deplacer(x, y):
+        pass
+
+    def attaquer(cible):
+        pass
