85ca6b9fc5addaad695b1d3ddb40775597837bde
Shiroiryu753 <146004908+Shiroiryu753@users.noreply.github.com>
2024-05-14 15:42:39 +0200

    Feat : Rajout des items : coeur,bottes,vin
    Rajout des items : coeur, bottes, vin + modification de Joueurs pour définir le fonctionnement des objets

--- Joueur.py
+++ Joueur.py
@@ -1,10 +1,11 @@
 class Joueur:
-    def __init__(self, listePerso, vie, inventaire, xp, niveau):
+    def __init__(self, listePerso, inventaire, xp, niveau, vie = 100):
         self.__listePerso = listePerso
         self.__vie = vie
         self.__inventaire = inventaire
         self.__niveau = niveau
         self.__xp = xp
+        self.__hauteur_saut = 1.0
 
         @property
         def get_level(self):
@@ -13,10 +14,38 @@ class Joueur:
         @property
         def get_vie(self):
             return self.__vie
+        
+        @setter.vie
+        def vie(self, valeur):
+            if valeur > 0:
+                self.__vie = valeur
+            else:
+                self.__vie = 0
+                print("Le joueur est mort.")
+            print(f"Vie actuelle: {self.__vie}")
+        
+        def modifier_vie(self, quantite):
+            if self.vie + quantite >= 0:
+                self.vie += quantite  # Utilisation du setter
+            else:
+                self.vie = 0  # Assurez que la vie ne passe pas en-dessous de 0
+                print("Le joueur est mort.")
 
         @property
         def get_inventaire(self):
             return self.__inventaire
+
+        @property
+        def hauteur_saut(self):
+            return self.__hauteur_saut
+
+        @hauteur_saut.setter
+        def hauteur_saut(self, valeur):
+            self.__hauteur_saut = valeur
+
+        def modifier_saut(self, multiplicateur):
+            self.hauteur_saut *= multiplicateur
+            print(f"Hauteur de saut multipliée par : {self.hauteur_saut}")
 
         # @property
         # def niveau(self):
--- Objet.py
+++ Objet.py
@@ -1,24 +1,57 @@
+import Joueur
+import Ennemi
+
 class objet:
     def __init__(self, nom, type):
         self.__nom = nom
         self.__type = type
+        self.__chap = "chapeau"
+        self.__cle = "cle"
+        self.__coeur = "coeur"
+        self.__bottes = "bottes"
+        self.__vin = "vin"
+        self.__epee = "epee"
+        self.__hache = "hache"
+        self.__arc = "arc"
 
-        @property
-        def nom(self):
-            return self.__nom
-        @nom.setter
-        def nom(self, valeur):
-            self.__nom = valeur
+    @property
+    def coeur(self):
+        return self.__coeur
+    
+    @coeur.setter
+    def coeur(self, valeur):
+        self.__coeur = valeur
+    
+    @property
+    def vin(self):
+        return self.__vin
+    
+    @coeur.setter
+    def coeur(self, valeur):
+        self.__vin = valeur
+    
+    @property
+    def type(self):
+        return self.__type
+    @property
+    def bottes(self):
+        return self.__bottes
+    
+    @bottes.setter
+    def bottes(self, valeur):
+        self.__bottes = valeur
 
-        @property
-        def type(self, valeur):
-            return self.__type
+    @type.setter
+    def type(self, valeur):
+        self.__type = valeur
 
-        @type.setter
-        def type(self, valeur):
-            self.__type = valeur
+    def UtiliserObjet(self, joueur):
+        if self.__nom == "coeur":
+            joueur.modifier_vie(50)
+        elif self.__nom == "vin":
+            joueur.modifier_vie(25)
+        elif self.__nom == "bottes":
+            joueur.modifier_saut(1.5)  # Cette méthode doit être ajoutée à la classe Joueur
 
-    def UtiliserObjet(self):
-        pass
     def LacherObjet(self):
-        pass
+        print("Objet lâché.")
