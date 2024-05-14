2ed7717d263bc01baa25bcca0767f19f56cd5767
Shiroiryu753 <146004908+Shiroiryu753@users.noreply.github.com>
2024-05-14 11:27:15 +0200

    Modification majeur du menu pause + ajout menu principale

--- Menu.py
+++ Menu.py
@@ -1,106 +1,123 @@
 import pygame
+import os
+from Interface import Interface
+from Audio import Audio
 
-def pause_menu(window, clock):
-    pygame.font.init()
-    font = pygame.font.Font(None, 36)
-    title_font = pygame.font.Font(None, 50)
-    window_width, window_height = window.get_size()
+class MenuPrincipal:
+    def __init__(self, window):
+        self.window = window
+        self.window_width, self.window_height = self.window.get_size()
+        self.font = pygame.font.Font(None, 36)
 
-    # Dimensions et positionnement du menu
-    menu_width = 450
-    menu_height = 400  
-    menu_x = (window_width - menu_width) // 2
-    menu_y = (window_height - menu_height) // 2
-    
-    button_width = 260
-    button_height = 50
-    button_x = menu_x + (menu_width - button_width) // 2
-    
-    title_height = 60
-    title_y = menu_y + 20
-    
+        self.audio = Audio()
+        self.audio.musicAmbiance("menu.mp3", True)
 
-    button_positions = [
-        title_y + title_height + 20,
-        title_y + title_height + 80,
-        title_y + title_height + 200,  
-        title_y + title_height + 260  
-    ]
-    
-    buttons = {
-        'reprendre': pygame.Rect(button_x, button_positions[0], button_width, button_height),
-        'sauver': pygame.Rect(button_x, button_positions[1], button_width, button_height),
-        'quitter': pygame.Rect(button_x, button_positions[3], button_width, button_height)
-    }
-    
-    texts = {
-        'reprendre': font.render('Retour en jeu', True, (0, 0, 0)),
-        'sauver': font.render('Sauvegarder la partie', True, (0, 0, 0)),
-        'quitter': font.render('Quitter le jeu', True, (0, 0, 0))
-    }
-    
-    # Control du volume
-    volume_label = font.render('Son', True, (0, 0, 0))
-    volume_label_rect = volume_label.get_rect(midtop=(menu_x + menu_width // 2, button_positions[1] + 70))
-    
-    volume_rect = pygame.Rect(button_x, button_positions[1] + 110, button_width, 20)
-    volume_level = 1.0  # Volume initial
+        # Set background to menuBackground.png
+        background = pygame.image.load(os.path.join("assets", "img", "menuBackground.png"))
+        self.window.blit(background, (0, 0))
 
-    pause_text = title_font.render('PAUSE', True, (0, 0, 0))
-    pause_text_rect = pause_text.get_rect(center=(menu_x + menu_width // 2, title_y + title_height // 2))
-    
-    menu_active = True
-    adjusting_volume = False
-    
-    while menu_active:
-        pygame.draw.rect(window, (255, 255, 255), (menu_x, menu_y, menu_width, menu_height), border_radius=4)
-        pygame.draw.rect(window, (200, 200, 200), (menu_x, title_y, menu_width, title_height))
-        
-        window.blit(pause_text, pause_text_rect)
-        
-        for key, button in buttons.items():
-            text_rect = texts[key].get_rect(center=button.center)
-            window.blit(texts[key], text_rect)
+        fontTitle = pygame.font.SysFont(None, 80)
 
-        # Affichier le volume
-        window.blit(volume_label, volume_label_rect)
-        pygame.draw.rect(window, (200, 200, 200), volume_rect)
-        handle_x = volume_rect.x + int(volume_level * (volume_rect.width - 20))
-        handle_rect = pygame.Rect(handle_x, volume_rect.y, 20, volume_rect.height)
-        pygame.draw.rect(window, (100, 100, 100), handle_rect)
+        # Render title text in white
+        title_surface = fontTitle.render('La Conquête Pirate !', True, (255, 255, 255))
 
+        # Get the rectangle of the text and center it
+        title_rect = title_surface.get_rect(center=(self.window_width // 2, 200))
+
+        # Blit the title text on the window
+        self.window.blit(title_surface, title_rect)
+
+        self.button_width = 350
+        self.button_height = 50
+        self.button_margin = 5  # Margin for the black background
+        self.button_spacing = 20  # Additional spacing between buttons
+
+        save_slots = [] # (eg. ["1", "2", "3"])
+
+        # Calculate the starting y position to center all buttons
+        total_button_height = (self.button_height + self.button_margin + self.button_spacing) * (2 + len(save_slots)) - self.button_spacing
+        start_y = (self.window_height - total_button_height) // 2
+
+        self.buttons = {
+            'play': pygame.Rect((self.window_width - self.button_width) // 2, 
+                                start_y, 
+                                self.button_width, self.button_height),
+            'quit': pygame.Rect((self.window_width - self.button_width) // 2, 
+                                start_y + (self.button_height + self.button_margin + self.button_spacing) * (1 + len(save_slots)), 
+                                self.button_width, self.button_height)
+        }
+        if len(save_slots) > 0:
+            for i, slot in enumerate(save_slots):
+                self.buttons[f'load_{slot}'] = pygame.Rect(
+                    (self.window_width - self.button_width) // 2, 
+                    start_y + (self.button_height + self.button_margin + self.button_spacing) * (i + 1), 
+                    self.button_width, self.button_height
+                )
+
+        self.texts = {
+            'play': self.font.render('Lancer une partie', True, (255, 255, 255)),
+            'quit': self.font.render('Quitter', True, (255, 255, 255))
+        }
+
+        for slot in save_slots:
+            self.texts[f'load_{slot}'] = self.font.render(f'Charger la sauvegarde #{slot}', True, (255, 255, 255))
+
+        self.button_clicked = False  # Initialize the button click state
+        self.click_timer = 0  # Initialize the click timer
+
+    def draw(self):
+        for key, button in self.buttons.items():
+            # Draw black background
+            black_rect = pygame.Rect(button.left - self.button_margin,
+                                     button.top - self.button_margin,
+                                     button.width + 2 * self.button_margin,
+                                     button.height + 2 * self.button_margin)
+            pygame.draw.rect(self.window, (0, 90, 90), black_rect)
+
+            # Draw button
+            pygame.draw.rect(self.window, (0, 128, 128), button)
+
+            # Draw text
+            text_rect = self.texts[key].get_rect(center=button.center)
+            self.window.blit(self.texts[key], text_rect)
         pygame.display.update()
-        
+
+    def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  
+            elif event.type == pygame.MOUSEBUTTONDOWN and not self.button_clicked:
+                if event.button == 1:
                     pos = pygame.mouse.get_pos()
-                    for key, button in buttons.items():
+                    for key, button in self.buttons.items():
                         if button.collidepoint(pos):
-                            if key == 'reprendre':
-                                return True
-                            elif key == 'sauver':
-                                print("Sauvegarde du jeu...")
-                            elif key == 'quitter':
+                            self.button_clicked = True  # Set the flag to True when a button is clicked
+                            self.click_timer = pygame.time.get_ticks()  # Record the time of the click
+                            if key == 'play':
+                                return 'play'
+                            elif key == 'quit':
                                 pygame.quit()
                                 exit()
-                    if volume_rect.collidepoint(pos):
-                        adjusting_volume = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:  
-                    adjusting_volume = False
-            elif event.type == pygame.MOUSEMOTION:
-                if adjusting_volume:
-                    relative_x = event.pos[0] - volume_rect.x
-                    volume_level = max(0, min(1, relative_x / (volume_rect.width - 20)))
-                    pygame.mixer.music.set_volume(volume_level) 
-                
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return True 
-        
-        clock.tick(60)
-
+                            elif key.startswith('load_'):
+                                save_slot = key.split('_')[1]
+                                return f'load_{save_slot}'
+        return None
+    
+    def run(self):
+        menu_active = True
+        while menu_active:
+            self.draw()
+            action = self.handle_events()
+            if action == 'play':
+                self.audio.stopMusic()  # Stop the music before launching the interface
+                menu_active = False
+                print("Lancement du jeu...")
+                interface = Interface(self.window)
+                interface.run()
+            elif action and action.startswith('load_'):
+                save_slot = action.split('_')[1]
+                print(f"On charge la sauvegarde #{save_slot}")
+            pygame.time.wait(20)
+            if self.button_clicked and pygame.time.get_ticks() - self.click_timer > 100:  # 100 ms delay
+                self.button_clicked = False
