ece5c74e90ca1dd099c5265ba60033b7589d0fc3
the-seven237 <111310673+the-seven237@users.noreply.github.com>
2024-05-14 15:46:55 +0200

    feat
    affichage de la maps en full screen

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from menu import pause_menu
 
 from Interface import Interface
 from Audio import Audio
+from Carte import carte  # Import the carte class from Carte.py
+
 pygame.init()
 
 width = 1920
@@ -15,28 +17,34 @@ window = pygame.display.set_mode(win, pygame.FULLSCREEN)
 
 interface = Interface(window)
 audio = Audio()
+map = carte(1920, 1080, 'assets/img/maps.jpg', 1)  # Create a carte instance with image path
+
 
 def quit_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        elif event.type == pygame.KEYDOWN:
+        elif event.type is pygame.KEYDOWN:
             if event.key == pygame.K_p:
                 pygame.quit()
                 exit()
             elif event.key == pygame.K_ESCAPE:
                 pause_menu(window, clock)
+
+
 def main():
     run = True
     while run:
         quit_game()
-        
+
         window.fill((0, 0, 0))
+        map.affichercarte(window)  # Display the map
         interface.draw()
-        
+
         pygame.display.update()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
