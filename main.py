0fe0c2ffebe8ae4f22b3b58d924bb2759d1b556e
Shiroiryu753 <146004908+Shiroiryu753@users.noreply.github.com>
2024-05-14 11:28:46 +0200

    Modification mineur

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 import pygame
-from Menu import pause_menu
+from Menu import MenuPrincipal
 
-from Interface import Interface
 pygame.init()
 
 width = 1920
 height = 1080
-win = ((width, height))
+win = (width, height)
 clock = pygame.time.Clock()
 
 pygame.display.set_caption("La Conquête Pirate")
 window = pygame.display.set_mode(win, pygame.FULLSCREEN)
-
-interface = Interface(window)
 
 def quit_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:
-                pygame.quit()
-                exit()
-            elif event.key == pygame.K_ESCAPE:
-                pause_menu(window, clock)
+            
 def main():
-    
     run = True
     while run:
         quit_game()
         
         window.fill((0, 0, 0))
-        interface.draw()
+        menu = MenuPrincipal(window)
+        menu.run()
         
         pygame.display.update()
         clock.tick(60)
