bf0ffbcf4358e920cec2f58220cf9a840f84e747
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2024-05-14 21:11:11 +0200

    v1.3.4

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pygame
-from menu import pause_menu
 
-from Interface import Interface
-from Audio import Audio
-from Carte import carte  # Import the carte class from Carte.py
+from Menu import MenuPrincipal
 
 pygame.init()
 
@@ -15,36 +12,23 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("La Conquête Pirate")
 window = pygame.display.set_mode(win, pygame.FULLSCREEN)
 
-interface = Interface(window)
-audio = Audio()
-map = carte(1920, 1080, 'assets/img/img3.png', 1)  # Create a carte instance with image path
-
-
 def quit_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        elif event.type is pygame.KEYDOWN:
-            if event.key == pygame.K_p:
-                pygame.quit()
-                exit()
-            elif event.key == pygame.K_ESCAPE:
-                pause_menu(window, clock)
-
-
+            
 def main():
     run = True
     while run:
         quit_game()
-
+        
         window.fill((0, 0, 0))
-        map.affichercarte(window)  # Display the map
-        interface.draw()
-
+        menu = MenuPrincipal(window)
+        menu.run()
+        
         pygame.display.update()
         clock.tick(60)
-
 
 if __name__ == "__main__":
     main()
